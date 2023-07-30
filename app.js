ef8373294b772327967a7177575d7207af94b56e
Justin V <127573153+JustVDev@users.noreply.github.com>
2023-07-30 16:33:48 -0500

    Start JS functionality

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
-//  start new game
-//  || take user input ||
-//  || computer generate choice ||
-//  || find round roundWinner ||
-//  || output message ||
-//  change score
-//  check for game win
+const CHOICES = ['ROCK', 'PAPER', 'SCISSORS'];
+const MAXPOINTS = 5;
 
-//  take user input and repeat
-
-const choices = ['rock', 'paper', 'scissors'];
-const maxRounds = 5;
+const computerChoiceDom = document.querySelector('.computer-choice');
+const userScoreDOM = document.querySelector('.player-score');
+const computerScoreDOM = document.querySelector('.cpu-score');
 
 
+let userChoice;
+let computerChoice;
+
+const btns = document.querySelectorAll('button');
+
+let userScore = 0;
+let computerScore = 0;
 
 //game function
-// game();
+game();
 
-// functions below
-
-function getUserInput() {
-	const userInput = prompt(
-		'Please enter your choice (rock, paper, scissors)'
-	).toLowerCase();
-	if (userInput != 'rock' && userInput != 'paper' && userInput != 'scissors') {
-		console.log('please only use rock paper or scissors.');
+function game (){
+	btns.forEach((btn) => {
+		btn.addEventListener('click', () => {
+			userChoice = btn.dataset.type;
+			computerChoice = getComputerInput();
+			console.log(userChoice, computerChoice)
+			playRound(userChoice, computerChoice);
+		})
+	});
+	if(userScore >= MAXPOINTS){
+		computerChoiceDom.textContent = 'USER WON!'
+	} else if (computerScore >= MAXPOINTS){
+		computerChoiceDom.textContent = 'CPU Won!'
 	}
-	return userInput;
+	
+	
+	
 }
 
-function getComputerChoice() {
-	const choice = choices[Math.floor(Math.random() * 3)];
+
+
+function getComputerInput() {
+	const choice = CHOICES[Math.floor(Math.random() * 3)];
 	return choice;
 }
 
-function playRound(computer, user) {
+function playRound(user, computer) {
 	if (computer === user) {
 		console.log('Tie');
+		computerChoiceDom.textContent = `Computer Choice = ${computer}`;
 		return 'tie';
 		// user wins
 	} else if (
@@ -43,43 +54,54 @@ function playRound(computer, user) {
 		(computer === 'PAPER' && user === 'SCISSORS') ||
 		(computer === 'SCISSORS' && user === 'ROCK')
 	) {
+		userScore++;
+		userScoreDOM.textContent = `User: ${userScore}`;
+		computerChoiceDom.textContent = `Computer Choice = ${computer}`;
+
 		return 'user';
 	} else if (
 		(computer === 'ROCK' && user === 'SCISSORS') ||
 		(computer === 'PAPER' && user === 'ROCK') ||
 		(computer === 'SCISSORS' && user === 'PAPER')
 	) {
+		computerScore++;
+		computerScoreDOM.textContent =`${computerScore}: Computer`;
+		computerChoiceDom.textContent = `Computer Choice = ${computer}`;
+
 		return 'cpu';
 	}
 }
 
-function game() {
-	let userScore = 0;
-	let cpuScore = 0;
-	const gameWinner = undefined;
+// function game() {
+// 	let userScore = 0;
+// 	let cpuScore = 0;
+// 	const gameWinner = undefined;
 
-	for (let i = 0; i < maxRounds; i++) {
-		const computerChoice = getComputerChoice().toUpperCase();
-		const userInput = getUserInput().toUpperCase();
-		let roundWinner = playRound(computerChoice, userInput);
-		switch (roundWinner) {
-			case 'cpu':
-				cpuScore++;
-				break;
-			case 'user':
-				userScore++;
-				break;
-			case 'tie':
-				break;
-		}
-	}
-	console.table(userScore, cpuScore);
-	if(cpuScore > userScore){
-		console.log('CPU Won')
-	} else if (userScore > cpuScore){
-		console.log("User won!")
-	} else {
-		console.log('it was a tie')
-	}
+// 	for (let i = 0; i < maxRounds; i++) {
+// 		const computerChoice = getComputerInput().toUpperCase();
+// 		const userInput = getUserInput().toUpperCase();
+// 		let roundWinner = playRound(computerChoice, userInput);
+// 		switch (roundWinner) {
+// 			case 'cpu':
+// 				cpuScore++;
+// 				break;
+// 			case 'user':
+// 				userScore++;
+// 				break;
+// 			case 'tie':
+// 				break;
+// 		}
+// 	}
+// 	console.table(userScore, cpuScore);
+// 	if(cpuScore > userScore){
+// 		console.log('CPU Won')
+// 	} else if (userScore > cpuScore){
+// 		console.log("User won!")
+// 	} else {
+// 		console.log('it was a tie')
+// 	}
 	
-}
+// }
+
+
+year.textContent =  new Date().getFullYear()
